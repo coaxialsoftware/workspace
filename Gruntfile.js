318b9eebd948abb21f678b3bd36a103625bbc19c
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2014-10-24 15:47:31 -0400

    Removed tern

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,20 +28,6 @@ module.exports = function(grunt) {
 
 		concat: {
 
-			tern: {
-				src: [
-					'node_modules/tern/node_modules/acorn/acorn.js',
-					'node_modules/tern/node_modules/acorn/acorn_loose.js',
-					'node_modules/tern/node_modules/acorn/util/walk.js',
-					'node_modules/tern/lib/signal.js',
-					'node_modules/tern/lib/tern.js',
-					'node_modules/tern/lib/def.js',
-					'node_modules/tern/lib/jsdoc.js',
-					'node_modules/tern/lib/infer.js'
-				],
-				dest: 'public/build/tern.js'
-			},
-
 			css: {
 				src: [
 					'client/styles.css'
