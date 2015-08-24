9d9424ab2edc066393f5b55da40c8ac6f843a38b
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2015-08-24 16:23:22 -0400

    Added login command

--- client/core.online.js
+++ client/core.online.js
@@ -9,6 +9,18 @@ var LoginDialog = ide.Editor.extend({
 	_setup: function()
 	{
 		this.$el.html(cxl.id(this.templateUrl).innerHTML);
+		
+		this.listenTo(this.$el.find('form'), 'submit', this.submit);
+	},
+	
+	submit: function(ev)
+	{
+		ide.socket.send('online', {
+			u: this.$el.find('[name="username"]').val(),
+			p: this.$el.find('[name="password"]').val()
+		});
+		
+		ev.preventDefault();
 	}
 });
 	
