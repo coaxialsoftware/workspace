46d2583a32f84a7d59fee9057f737fcc43e1aa78
Giancarlo Bellido <giancarlo@coaxialhost.com>
2017-06-07 21:14:37 +0000

    Added benchmark tests

---
+++ test/client/benchmark.js
@@ -0,0 +1,25 @@
+
+QUnit.module('benchmark');
+
+function perf(a, count, maxTime, fn)
+{
+	var time = performance.now();
+	console.profile();
+
+	while (count--)
+		fn();
+
+	console.profileEnd();
+	time = performance.now() - time;
+
+	a.ok(time < maxTime, 'Time: ' + time + ', Max Time: ' + maxTime);
+}
+
+QUnit.test('Hint rendering', function(a) {
+
+	perf(a, 10000, 120, function() {
+		var x = new ide.Hint({ title: 'Testing Performance' });
+		x.render();
+	});
+
+});
