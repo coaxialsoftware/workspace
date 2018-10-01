1393d4d57a291ecc623cad8e952da525ab485de4
giancarlo <giancarlo@coaxialhost.com>
2018-10-01 13:26:13 +0000

    Added build script

---
+++ scripts/build.js
@@ -0,0 +1,73 @@
+
+const
+	builder = require('@cxl/build'),
+
+	SRC = [
+		'client/core.js',
+		'client/core.item.js',
+		'client/core.editor.js',
+		'client/core.plugins.js',
+		'client/core.cmd.js',
+		'client/core.keymap.js',
+		'client/core.worker.js',
+		'client/core.workspace.js',
+		'client/core.socket.js',
+		'client/core.diff.js',
+		'client/core.file.js',
+		'client/core.folder.js',
+		'client/core.project.js',
+		'client/core.bar.js',
+		'client/core.source.js',
+		'client/core.welcome.js',
+		'client/core.assist.js'
+	],
+
+	LIBS = [
+		'node_modules/codemirror/lib/codemirror.js',
+		'node_modules/codemirror/addon/search/searchcursor.js',
+		'node_modules/codemirror/addon/search/find.js',
+		'node_modules/codemirror/addon/fold/xml-fold.js',
+		'node_modules/codemirror/addon/edit/matchbrackets.js',
+		'node_modules/codemirror/addon/edit/matchtags.js',
+		'node_modules/codemirror/addon/edit/closetag.js',
+		'node_modules/codemirror/addon/edit/closebrackets.js',
+		'node_modules/codemirror/addon/comment/comment.js',
+		'node_modules/codemirror/addon/comment/continuecomment.js',
+		'node_modules/codemirror/addon/fold/foldcode.js',
+		'node_modules/codemirror/addon/fold/foldgutter.js',
+		'node_modules/codemirror/addon/fold/brace-fold.js',
+		'node_modules/codemirror/addon/selection/active-line.js',
+		'node_modules/codemirror/addon/mode/overlay.js',
+		'node_modules/codemirror/mode/meta.js',
+		'node_modules/codemirror/mode/javascript/javascript.js',
+		'node_modules/codemirror/mode/xml/xml.js',
+		'node_modules/codemirror/mode/htmlmixed/htmlmixed.js',
+		'node_modules/codemirror/mode/css/css.js',
+
+		'node_modules/xterm/dist/xterm.js',
+		'node_modules/xterm/dist/addons/fit/fit.js',
+
+		'node_modules/@cxl/ui/dist/index.js',
+	]
+;
+
+builder.build({
+
+	outputDir: 'public/build-test',
+	targets: [
+		{
+			outputs: 'libs.js',
+			src: [ ...LIBS ]
+		},
+		{
+			output: 'debug.js',
+			src: [ ...SRC, 'client/debug.js' ]
+		},
+		{
+			output: 'ide.js',
+			src: [ ...LIBS, ...SRC],
+			minify: 'ide.min.js'
+		}
+	]
+
+});
